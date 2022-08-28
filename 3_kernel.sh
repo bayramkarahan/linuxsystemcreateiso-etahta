6ac1524deab09de3ce5ee9157d266e4d066b1f5f
karahan <bayramk@gmail.com>
2022-08-28 15:19:16 +0300

    kernel

--- 3_kernel.sh
+++ 3_kernel.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #### liquorix kernel
-curl https://liquorix.net/liquorix-keyring.gpg | chroot chroot apt-key add -
-echo "deb http://liquorix.net/debian testing main" > chroot/etc/apt/sources.list.d/liquorix.list
-chroot chroot apt-get update -y
-chroot chroot apt-get install linux-image-liquorix-amd64 -y
-chroot chroot apt-get install linux-headers-liquorix-amd64 -y
+#curl https://liquorix.net/liquorix-keyring.gpg | chroot chroot apt-key add -
+#echo "deb http://liquorix.net/debian testing main" > chroot/etc/apt/sources.list.d/liquorix.list
+#chroot chroot apt-get update -y
+#chroot chroot apt-get install linux-image-liquorix-amd64 -y
+#chroot chroot apt-get install linux-headers-liquorix-amd64 -y
 
 #### stock kernel 
-#chroot chroot apt-get install linux-image-amd64 -y
-#chroot chroot apt-get install linux-headers-amd64 -y
+chroot chroot apt-get update -y
+chroot chroot apt-get install linux-image-amd64 -y
+chroot chroot apt-get install linux-headers-amd64 -y
