f369d6d7e3bbc9c0d244a0b17ff156bb54caea8f
karahan <bayramk@gmail.com>
2022-10-02 19:06:29 +0300

    repo

--- 2_createsystem.sh
+++ 2_createsystem.sh
@@ -14,8 +14,8 @@ apt-get install git devscripts equivs -y
 mkdir chroot  # || true
 ##### For debian
 
-#debootstrap --arch=amd64 --no-merged-usr sid chroot https://deb.debian.org/debian
-#echo 'deb https://deb.debian.org/debian sid main contrib non-free' > chroot/etc/apt/sources.list
+debootstrap --arch=amd64 --no-merged-usr sid chroot https://deb.debian.org/debian
+echo 'deb https://deb.debian.org/debian sid main contrib non-free' > chroot/etc/apt/sources.list
 
 #debootstrap --arch=amd64 --no-merged-usr testing chroot https://deb.debian.org/debian
 #echo 'deb https://deb.debian.org/debian testing main contrib non-free' > chroot/etc/apt/sources.list
@@ -23,8 +23,8 @@ mkdir chroot  # || true
 #debootstrap --arch=amd64 --no-merged-usr stable chroot https://deb.debian.org/debian
 #echo 'deb https://deb.debian.org/debian stable main contrib non-free' > chroot/etc/apt/sources.list
 
-debootstrap --arch=amd64 --no-merged-usr yirmibir chroot https://depo.pardus.org.tr/pardus
-echo 'deb https://depo.pardus.org.tr/pardus yirmibir main contrib non-free' > chroot/etc/apt/sources.list
+#debootstrap --arch=amd64 --no-merged-usr yirmibir chroot https://depo.pardus.org.tr/pardus
+#echo 'deb https://depo.pardus.org.tr/pardus yirmibir main contrib non-free' > chroot/etc/apt/sources.list
 
 #debootstrap --arch=amd64 --no-merged-usr ondokuz chroot https://19.depo.pardus.org.tr/etap
 #echo 'deb https://19.depo.pardus.org.tr/etap ondokuz main contrib non-free' > chroot/etc/apt/sources.list
