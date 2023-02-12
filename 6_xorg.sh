dea44f16513d5a61a951d18e0b7d35357a3d82f4
bayramkarahan <bayramk@gmail.com>
2023-02-13 00:49:52 +0300

    wallpaper

--- 6_xorg.sh
+++ 6_xorg.sh
@@ -46,6 +46,7 @@ wget -O greeter-keyboard.deb  https://github.com/bayramkarahan/pardus-lightdm-gr
 wget -O greeter-sshlogin.deb  https://github.com/bayramkarahan/pardus-lightdm-greeter-sshlogin/releases/download/current/pardus-lightdm-greeter-sshlogin_1.0_all.deb
 wget -O greeter-qrcode.deb  https://github.com/bayramkarahan/pardus-lightdm-greeter-qrcode/releases/download/current/pardus-lightdm-greeter-qrcode_1.0_all.deb
 wget -O greeter-ebaqr-ogretmen.deb  https://github.com/bayramkarahan/pardus-lightdm-greeter-ebaqr-ogretmen/releases/download/current/pardus-lightdm-greeter-ebaqr-ogretmen_1.0_all.deb
+wget -O osk.deb  https://github.com/bayramkarahan/gtk-keyboard-osk/releases/download/current/osk_0.1.0_all.deb
 
 mv greeter.deb chroot/tmp
 chroot chroot dpkg -i /tmp/greeter.deb # dosya adını uygun şekilde yazınız.
@@ -57,5 +58,7 @@ mv greeter-qrcode.deb chroot/tmp
 chroot chroot dpkg -i /tmp/greeter-qrcode.deb # dosya adını uygun şekilde yazınız.
 mv greeter-ebaqr-ogretmen.deb chroot/tmp
 chroot chroot dpkg -i /tmp/greeter-ebaqr-ogretmen.deb # dosya adını uygun şekilde yazınız.
+mv osk.deb chroot/tmp
+chroot chroot dpkg -i /tmp/osk.deb # dosya adını uygun şekilde yazınız.
 chroot chroot apt --fix-broken install -y
 chroot chroot apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
