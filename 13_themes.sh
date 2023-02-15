93e2fdda75d3c262b7621bcba9cdd6b2daab1b71
bayramkarahan <bayramk@gmail.com>
2023-02-15 11:52:21 +0300

    plymouth

--- 13_themes.sh
+++ 13_themes.sh
@@ -5,10 +5,11 @@ i#!/bin/bash
 echo "açılış teması yapılandırılıyor"
 
 #run install
-#chroot chroot apt install plymouth* -y
-#cp pardus.png chroot/usr/share/plymouth/themes/dolunay/pardus.png
+chroot chroot apt install plymouth -y
+cp etap.png chroot/usr/share/plymouth/themes/lines/debian.png
+cp logo.png chroot/usr/share/plymouth/themes/lines/logo.png
 
-#chroot chroot plymouth-set-default-theme -R dolunay
+chroot chroot plymouth-set-default-theme -R lines
 
 
 chroot chroot apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
