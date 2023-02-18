bc78b59877acfc5552389a9934d4a0071345de54
bayramkarahan <bayramk@gmail.com>
2023-02-18 19:23:43 +0300

    grub wallpaper

--- 13_themes.sh
+++ 13_themes.sh
@@ -8,12 +8,18 @@ echo "açılış teması yapılandırılıyor"
 chroot chroot apt install plymouth* -y
 cp etahta.png chroot/usr/share/plymouth/themes/lines/debian.png
 cp logo.png chroot/usr/share/plymouth/themes/lines/logo.png
-cp duvar.svg chroot/etc/alternatives/desktop-theme/login/background.svg
-cp duvar.svg chroot/etc/alternatives/desktop-theme/login/background-nologo.svg
-cp duvar.svg chroot/etc/alternatives/desktop-theme/wallpaper/contents/images/1280x1024.svg
-cp duvar.svg chroot/etc/alternatives/desktop-theme/wallpaper/contents/images/1600x1200.svg
-cp duvar.svg chroot/etc/alternatives/desktop-theme/wallpaper/contents/images/1920x1080.svg
-cp duvar.svg chroot/etc/alternatives/desktop-theme/wallpaper/contents/images/1920x1200.svg
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/login/background.svg
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/login/background-nologo.svg
+
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/1280x1024.svg
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/1600x1200.svg
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/1920x1080.svg
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/wallpaper/contents/images/1920x1200.svg
+
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/lockscreen/contents/images/1280x1024.svg
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/lockscreen/contents/images/1600x1200.svg
+cp duvar.svg chroot/usr/share/desktop-base/emerald-theme/lockscreen/contents/images/1920x1080.svg
+cp duvar.svg chroot1920x1200.svg
 
 chroot chroot plymouth-set-default-theme -R lines
 
