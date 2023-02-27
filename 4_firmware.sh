cb29eb9ab14190c3a6a8f6c471b03a202b93579e
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2023-02-27 11:26:42 +0300

    Update 4_firmware.sh

--- 4_firmware.sh
+++ 4_firmware.sh
@@ -2,7 +2,7 @@
 chroot chroot apt-get install bluez-firmware firmware-amd-graphics firmware-atheros \
       firmware-b43-installer firmware-b43legacy-installer firmware-bnx2 \
       firmware-bnx2x firmware-brcm80211 firmware-cavium firmware-intel-sound \
-      firmware-intelwimax firmware-ipw2x00 firmware-ivtv firmware-iwlwifi \
+      firmware-ipw2x00 firmware-ivtv firmware-iwlwifi \
       firmware-libertas firmware-linux firmware-linux-free firmware-linux-nonfree \
       firmware-misc-nonfree firmware-myricom firmware-netxen firmware-qlogic \
       firmware-ralink firmware-realtek firmware-samsung firmware-siano \
