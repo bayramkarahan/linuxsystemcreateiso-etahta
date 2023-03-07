46a95951d4cb68cb6787c4848072c96b3248b2c9
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2023-03-07 15:56:54 +0300

    Update office.sh

--- office.sh
+++ office.sh
@@ -4,10 +4,10 @@
 apt --fix-broken install -y
 apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
 wget -O /tmp/wps.deb http://depo.pardus.org.tr/etap/pool/non-free/w/wps-office/wps-office_10.1.0.6757pardus2_amd64.deb
-#apt install libreoffice-calc -y
-#apt install libreoffice-writer -y
-#apt install libreoffice-impress -y
-#apt install libreoffice-l10n-tr -y
+apt install libreoffice-calc -y
+apt install libreoffice-writer -y
+apt install libreoffice-impress -y
+apt install libreoffice-l10n-tr -y
 
 #wget -O /tmp/wps.deb https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/11664/wps-office_11.1.0.11664.XA_amd64.deb
 dpkg -i /tmp/wps.deb
